1ef22a128e7a67a5ab85e51057da897c7e7884cf
aadriianaa <anamariaadrianacosma@iesebre.com>
2024-09-25 19:18:17 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ES">
+    <head>
+        <meta charset="UTF-8">
+        <title>Demo ASIX</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+
+                echo "Hola mundo!";
+
+            ?>
+        </h1>
+    </body>
+</html>
